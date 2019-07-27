8f0ed70b2c5f9d0425148ecfd26e545f25e3201a
Rmaravanyika <rmaravanyika.rm@gmail.com>
2019-07-28 00:22:19 +0200

    website details

---
+++ blog/views.py
@@ -0,0 +1,72 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib import messages
+from django.utils import timezone
+
+from .models import Post, FreeQuote, Newletter
+from .forms import PostForm, FreequoteForm, NewletterForm
+
+
+def home(request):
+    form = FreequoteForm(request.POST or None)
+    success =False
+    if request.method == "POST":
+    	if form.is_valid():
+    		form.save()
+    		form = FreequoteForm()
+    		success = True
+    		messages.success(request, 'Your Free quote request has been submited. Thank you')
+
+    sub_form = NewletterForm(request.POST or None)
+    if request.method == "POST":
+        if sub_form.is_valid():
+            sub_form.save()
+            sub_form = NewletterForm()  
+    return render(request, 'home.html', {'form': form, 'sub_form': sub_form})	
+
+def about(request):
+    sub_form = NewletterForm(request.POST or None)
+    if request.method == "POST":
+        if sub_form.is_valid():
+            sub_form.save()
+            sub_form = NewletterForm()  
+    return render(request, 'about.html', {'sub_form': sub_form})	
+
+def service(request):
+    sub_form = NewletterForm(request.POST or None)
+    if request.method == "POST":
+        if sub_form.is_valid():
+            sub_form.save()
+            sub_form = NewletterForm()  
+    return render(request, 'services.html', {'sub_form': sub_form})	
+
+
+def contact(request):
+    form = PostForm(request.POST or None)
+    success = False
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            form = PostForm()
+            success =True
+            messages.success(request, 'Your Message has been submitted, our team will be intouch soon.')	
+    sub_form = NewletterForm(request.POST or None)
+
+    if request.method == "POST":
+        if sub_form.is_valid():
+            sub_form.save()
+            sub_form = NewletterForm()  
+    return render(request, 'contact.html', {'form': form, 'sub_form': sub_form})	
+
+
+def newletter_view(request):
+    form = NewletterForm(request.POST or None)
+    success =False
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            form = NewletterForm()
+            success = True
+            messages.success(request, 'Newletter request has been submitted')    
+    return render(request, 'newletter.html', {'form': form})
+# Create your views here.
